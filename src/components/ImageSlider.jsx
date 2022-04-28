c6ce5854ddba82d1ecf0e6983ba2edb802584dfa
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2022-04-28 14:49:40 -0400

    Add slider and Viewers

---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,128 @@
+import React from "react";
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import slider1 from "../images/slider-badag.jpg";
+import slider2 from "../images/slider-badging.jpg";
+import slider3 from "../images/slider-scale.jpg";
+import slider4 from "../images/slider-scales.jpg";
+
+function ImageSlider() {
+  const setting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+    <Carousel {...setting}>
+      <Wrap>
+        <a href="">
+          <img src={slider1} alt="" />
+        </a>
+      </Wrap>
+      <Wrap>
+        <a href="">
+          <img src={slider2} alt="" />
+        </a>
+      </Wrap>
+      <Wrap>
+        <a href="">
+          <img src={slider3} alt="" />
+        </a>
+      </Wrap>
+      <Wrap>
+        <a>
+          <img src={slider4} alt="" />
+        </a>
+      </Wrap>
+    </Carousel>
+  );
+}
+
+export default ImageSlider;
+
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+
+  a {
+    border-radius: 4px;
+    display: block;
+    position: relative;
+    margin: 10px;
+    cursor: pointer;
+
+    @media (max-width: 768px) {
+      padding: 0px;
+    }
+
+    img {
+      box-sizing: border-box;
+      width: 100%;
+      height: 100%;
+      border-radius: 4px;
+      box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+
+      @media (max-width: 768px) {
+        height: 200px;
+        object-fit: cover;
+      }
+
+      &:hover {
+        outline: 4px solid rgba(249, 249, 249, 0.8);
+        outline-offset: -4px;
+        transition: outline 0.3s ease 0s;
+
+        @media (max-width: 768px) {
+          padding: 4px;
+        }
+      }
+    }
+  }
+`;
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+
+  ul li button {
+    //not active
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: initial;
+  }
+
+  .slick-prev {
+    left: -75px;
+  }
+
+  .slick-next {
+    right: -75px;
+  }
+`;
---
+++ src/components/Viewers.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import styled from "styled-components";
+import disney from "../images/viewers-disney.png";
+import marvel from "../images/viewers-marvel.png";
+import national from "../images/viewers-national.png";
+import pixar from "../images/viewers-pixar.png";
+import starwars from "../images/viewers-starwars.png";
+import disneyV from "../videos/1564674844-disney.mp4";
+import marvelV from "../videos/1564676115-marvel.mp4";
+import nationalV from "../videos/1564676296-national-geographic.mp4";
+import starwarsV from "../videos/1608229455-star-wars.mp4";
+import pixarV from "../videos/1564676714-pixar.mp4";
+
+function Viewers() {
+  return (
+    <Container>
+      <Viewer img={disney} vid={disneyV} />
+      <Viewer img={marvel} vid={marvelV} />
+      <Viewer img={national} vid={nationalV} />
+      <Viewer img={pixar} vid={pixarV} />
+      <Viewer img={starwars} vid={starwarsV} />
+    </Container>
+  );
+}
+
+export default Viewers;
+
+function Viewer({ img, vid }) {
+  return (
+    <div>
+      <img src={img} alt="" />
+      <video autoPlay={true} loop={true} playsInline={true} muted={true}>
+        <source src={vid} type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+const Container = styled.div`
+  margin-top: 30px;
+  padding: 30px 0px 26px;
+  display: grid;
+  grid-gap: 25px;
+  gap: 25px;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+
+  @media (max-width: 768px) {
+    gap: 5px;
+  }
+
+  div {
+    padding-top: 56.25%;
+    border-radius: 10px;
+    box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    overflow: hidden;
+    position: relative;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+    outline: 4px solid rgba(249, 249, 249, 0.1);
+
+    @media (max-width: 768px) {
+      outline: 2px solid rgba(249, 249, 249, 0.1);
+      width: 80px;
+      height: 80px;
+    }
+
+    img {
+      inset: 0px;
+      display: block;
+      height: 100%;
+      object-fit: cover;
+      opacity: 1;
+      position: absolute;
+      transition: opacity 500ms ease-in-out 0s;
+      width: 105%;
+      z-index: 1;
+
+      @media (max-width: 768px) {
+        object-fit: contain;
+      }
+    }
+
+    video {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      inset: 0;
+      opacity: 0;
+      z-index: 0;
+
+      @media (max-width: 768px) {
+        // width: 100%;
+        // height: auto;
+        object-fit: cover;
+        display: none;
+      }
+    }
+
+    &:hover {
+      box-shadow: rgb(0 0 0 /80%) 0 40px 58px -16px,
+        rgb(0 0 0 / 72%) 0 30px 22px -10px;
+      transform: scale(1.05);
+      outline-color: rgba(249, 249, 249, 0.8);
+
+      video {
+        opacity: 1;
+      }
+    }
+  }
+`;
